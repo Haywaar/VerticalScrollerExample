1bd10a354f8269fd3f6ad05a8dfd3f94baf8bd24
sa.kazantsev <sa.kazantsev@corp.mail.ru>
2023-05-25 10:36:55 +0400

    Added events with priority

---
+++ Assets/Scripts/CustomEventBus/CallbackWithPriority.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CustomEventBus
+{
+    public class CallbackWithPriority
+    {
+        /// <summary>
+        /// Чем выше Priority, тем раньше вызовется ивент
+        /// </summary>
+        public readonly int Priority;
+        public readonly object Callback;
+
+        public CallbackWithPriority(int priority, object callback)
+        {
+            Priority = priority;
+            Callback = callback;
+        }
+    }
+}
--- Assets/Scripts/CustomEventBus/EventBus.cs
+++ Assets/Scripts/CustomEventBus/EventBus.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace CustomEventBus
 {
     public class EventBus : IService
     {
-        private Dictionary<string, List<object>> _signalCallbacks = new Dictionary<string, List<object>>();
+        private Dictionary<string, List<CallbackWithPriority>> _signalCallbacks = new Dictionary<string, List<CallbackWithPriority>>();
 
-        public void Subscribe<T>(Action<T> callback)
+        public void Subscribe<T>(Action<T> callback, int priority = 0)
         {
             string key = typeof(T).Name;
             if (_signalCallbacks.ContainsKey(key))
             {
-                _signalCallbacks[key].Add(callback);
+                _signalCallbacks[key].Add(new CallbackWithPriority(priority, callback));
             }
             else
             {
-                _signalCallbacks.Add(key, new List<object>() { callback });
+                _signalCallbacks.Add(key, new List<CallbackWithPriority>() { new (priority, callback) });
             }
+            
+            _signalCallbacks[key] = _signalCallbacks[key].OrderByDescending(x => x.Priority).ToList();
         }
 
         public void Invoke<T>(T signal)
@@ -28,7 +31,7 @@ namespace CustomEventBus
             {
                 foreach (var obj in _signalCallbacks[key])
                 {
-                    var callback = obj as Action<T>;
+                    var callback = obj.Callback as Action<T>;
                     callback?.Invoke(signal);
                 }
             }
@@ -39,7 +42,11 @@ namespace CustomEventBus
             string key = typeof(T).Name;
             if (_signalCallbacks.ContainsKey(key))
             {
-                _signalCallbacks[key].Remove(callback);
+                var callbackToDelete = _signalCallbacks[key].FirstOrDefault(x => x.Callback.Equals(callback));
+                if (callbackToDelete != null)
+                {
+                    _signalCallbacks[key].Remove(callbackToDelete);
+                }
             }
             else
             {
